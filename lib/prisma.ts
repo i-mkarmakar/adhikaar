acbf3812efebfa06820e9cee98c107e2a5b111e6
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-11-26 17:20:14 +0530

    feat(prisma): add prisma configuration and update schema for client generation

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '../app/generated/prisma';
+import { PrismaClient } from '../app/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-declare global {
-    var prisma: PrismaClient | undefined;
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
 }
 
-const prisma = globalThis.prisma || new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
 
-if (process.env.NODE_ENV === 'development') {
-    globalThis.prisma = prisma;
-}
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-export default prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
